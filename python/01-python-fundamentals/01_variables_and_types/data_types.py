b89713d12b58d00b850d2eb6f382876cf35f8782
ravindra reddy <79123724+ravindrakanapuram@users.noreply.github.com>
2026-08-21 16:43:01 +0530

    udpated data types

---
+++ python/01-python-fundamentals/01_variables_and_types/data_types.py
@@ -0,0 +1,23 @@
+# Numbers & core types
+
+my_int = 10
+my_flaot = 0.6
+my_bool = True
+my_complex = 2 + 3j
+my_none = None
+
+#Strings
+my_string = "Hello, World"
+
+#collections
+my_list = [1, 2, 3]
+my_tuple = (4, 5, 6)
+my_set = {7, 8, 9}
+my_dict = {"key1": "value1", "key2": "value2"}
+
+#type conversions
+
+int_to_flaot = float(my_int)
+str_to_int = int("100")
+set_from_list = set(my_list)
+list_from_tuple = list(my_tuple)
